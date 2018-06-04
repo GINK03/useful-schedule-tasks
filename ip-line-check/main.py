88d5c0158a42fe2c60ee67e2c537cedd1d6f6e2c
gkobayashi <gim.kobayashi@gmail.com>
2018-06-04 17:41:59 +0900

    update

--- ip-line-check/main.py
+++ ip-line-check/main.py
@@ -13,11 +13,11 @@ def job():
     ip = requests.get('https://api.ipify.org').text
     hostname = gethostname()
     payload = { "message": f'このコンピュータ({hostname})のGlobalIPは{ip}です' }
+    r = requests.post( url, headers=headers, params=payload) #, files=files)
     print( json.dumps(payload,indent=2, ensure_ascii=False ) )
   except Exception as ex:
     print(ex)
   #files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです。
-  #r = requests.post( url, headers=headers, params=payload) #, files=files)
 
 schedule.every().day.at("10:30").do(job)
 schedule.every().day.at("15:00").do(job)
