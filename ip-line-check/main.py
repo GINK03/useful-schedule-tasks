ec0f439e8a3fe9929ca9f1c7cec7b9899f26f780
gkobayashi <gim.kobayashi@gmail.com>
2018-06-04 17:40:36 +0900

    update

--- ip-line-check/main.py
+++ ip-line-check/main.py
@@ -8,13 +8,16 @@ def job():
   url = "https://notify-api.line.me/api/notify"
   token = open('secret.txt').read().strip()
   headers = {"Authorization" : "Bearer "+ token}
-
-  ip = requests.get('https://api.ipify.org').text
-  hostname = gethostname()
-  payload = { "message": f'このコンピュータ({hostname})のGlobalIPは{ip}です' }
-  print( json.dumps(payload,indent=2, ensure_ascii=False ) )
+  
+  try:
+    ip = requests.get('https://api.ipify.org').text
+    hostname = gethostname()
+    payload = { "message": f'このコンピュータ({hostname})のGlobalIPは{ip}です' }
+    print( json.dumps(payload,indent=2, ensure_ascii=False ) )
+  except Exception as ex:
+    print(ex)
   #files = {"imageFile": open("test.jpg", "rb")} #バイナリで画像ファイルを開きます。対応している形式はPNG/JPEGです。
-  r = requests.post( url, headers=headers, params=payload) #, files=files)
+  #r = requests.post( url, headers=headers, params=payload) #, files=files)
 
 schedule.every().day.at("10:30").do(job)
 schedule.every().day.at("15:00").do(job)
